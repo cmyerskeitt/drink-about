d10b57e1f86e496a562e737c90d2b4a8f3806f44
github username <cmyerskeitt@gmail.com>
2022-12-29 11:26:26 -0500

    update code from switch to esle if

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 function peopleWithAgeDrink(old) {
-    switch(old){
-        case (old < 14):
-          return "drink toddy";
-          break;
-        case (old > 14 && old < 18):
-          return "drink coke";
-          break;
-        case (old > 18 && old < 21):
-          return "drink beer";
-          break;
-        default:
-          return "drink whisky";
-          break;
-    }
-};
+  if (old < 14){
+    return "drink toddy"
+  } else if (old >= 14 && old < 18){
+     return "drink coke"
+  } else if (old >= 18 && old < 21){
+    return "drink beer"
+  } else{
+    return "drink whisky";
+  }
+ };
